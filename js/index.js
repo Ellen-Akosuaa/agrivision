7e9f4c21bbd3fff4dc88f95e35f5c95462206e14
Ellen Ntiamoah <ellenntiamoah@gmail.com>
2024-12-07 15:48:20 +0000

    Added Hamburger Menu

--- js/index.js
+++ js/index.js
@@ -49,3 +49,12 @@ form.addEventListener('submit', (event) => {
   // Reset the form
   form.reset();
 });
+
+// JavaScript to toggle the menu
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navLinks.classList.toggle('open');
+});
